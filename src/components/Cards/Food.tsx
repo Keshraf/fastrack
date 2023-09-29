b89f64c1b38e570a6a2376dfddbfaa9c3c7da6bc
Ketan Saraf <saraf.ketan1108@gmail.com>
2023-09-29 13:27:24 -0700

    feat: catalog

---
+++ src/components/Cards/Food.tsx
@@ -0,0 +1,32 @@
+export type Food = {
+  name: string;
+  vegetarian: boolean;
+  price: number;
+  description: string;
+  image: string;
+};
+
+const FoodCard = ({ name, vegetarian, price, description, image }: Food) => {
+  return (
+    <div className="w-full rounded-xl p-3 border-[2px] border-[#121212]">
+      <div
+        className="w-full h-20 rounded-lg overflow-hidden bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
+      ></div>
+      <div className="flex justify-between items-center w-full pt-3 pb-[2px]">
+        <h2 className="text-xl font-bold">{name}</h2>
+        <button className="bg-blue-400 rounded-md py-1 px-4 text-xs">
+          Edit
+        </button>
+      </div>
+
+      <p className="text-md text-white/70 font-semibold pb-[2px]">${price}</p>
+      {/* <p>{vegetarian ? "Vegetarian" : "Non-Vegetarian"}</p> */}
+      <p className="text-sm text-white/50">{description}</p>
+    </div>
+  );
+};
+
+export default FoodCard;
--- src/pages/dashboard/catalog.tsx
+++ src/pages/dashboard/catalog.tsx
@@ -1,9 +1,171 @@
+import FoodCard, { Food } from "@/components/Cards/Food";
+
+const calatag: Food[] = [
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description:
+      "It is a very good burger. It is made using the best ingredients.",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+  {
+    name: "Burger",
+    vegetarian: false,
+    price: 100,
+    description: "Burger",
+    image:
+      "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+  },
+];
+
 const DashboardCatalog = () => {
   return (
     <section className="flex flex-col items-start justify-start w-full h-screen p-9 overflow-y-auto">
       <h1 className="text-3xl font-bold text-center text-white-700">Catalog</h1>
       <h3 className="text-md text-center text-white/20">{"100 Items"}</h3>
-      <section className="w-full"></section>
+      <section className="w-full py-6 grid grid-cols-4 grid-flow-row gap-4">
+        {calatag.map((food, index) => {
+          return <FoodCard key={index} {...food} />;
+        })}
+      </section>
     </section>
   );
 };
